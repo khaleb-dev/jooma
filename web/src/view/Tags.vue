358170597d0aca80384609896fda463fb4bdb595
Khaleb O'Brien <ebukauche52@gmail.com>
2021-05-20 20:57:55 +0100

    UI - tagData

--- web/src/view/Tags.vue
+++ web/src/view/Tags.vue
@@ -1,24 +1,51 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col-12">
-        <Tags></Tags>
+    <div class="row" v-if="!isTagSelected">
+      <div class="col-xl-4">
+        <div class="card card-custom card-stretch gutter-b" @click="showTagData(1)">
+          <div class="card-body d-flex flex-column p-0">
+            <div class="d-flex align-items-center justify-content-between card-spacer">
+              <div class="d-flex flex-column mr-2">
+                <a href="#" class="text-dark-75 text-hover-warning font-weight-bolder font-size-h5">Tag name</a>
+                <span class="text-muted font-weight-bold mt-2">Description</span>
+              </div>
+              <span class="symbol symbol-light-warning symbol-45">
+                <span class="symbol-label font-weight-bolder font-size-h6">18</span>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+    <div v-else>
+      <TagData></TagData>
     </div>
   </div>
 </template>
 
 <script>
 import { SET_BREADCRUMB } from "@/core/services/store/breadcrumbs.module";
-import Tags from "@/view/components/Tags.vue";
+import TagData from "@/view/components/TagData.vue";
 
 export default {
   name: "tags",
   components: {
-    Tags
+    TagData
+  },
+  data () {
+    return {
+      isTagSelected: false,
+      selectedType: ''
+    }
   },
   mounted() {
     this.$store.dispatch(SET_BREADCRUMB, [{ title: "Tags" }]);
   },
+  methods: {
+    showTagData: function(tagId){
+      this.selectedType = tagId,
+      this.isTagSelected = true
+    }
+  }
 };
 </script>
---
+++ web/src/view/components/TagData.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card card-custom card-stretch gutter-b">
+        <div class="card-body d-flex flex-column p-0">
+          <div class="d-flex align-items-center justify-content-between card-spacer">
+            <div class="d-flex flex-column mr-2">
+              <a href="#" class="text-dark-75 text-hover-warning font-weight-bolder font-size-h5">Selected Tag name</a>
+              <span class="text-muted font-weight-bold mt-2">Description</span>
+            </div>
+            <span class="symbol symbol-light-warning symbol-45">
+              <span class="symbol-label font-weight-bolder font-size-h6">18</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card card-custom gutter-b col-12">
+      <div class="card-body pt-5 pb-0 mt-n3">
+        <div class="table-responsive">
+          <table class="table table-borderless table-vertical-center">
+            <tbody>
+              <tr>
+                <td class="p-0 py-4">
+                  <div class="symbol symbol-60 symbol-2by3 flex-shrink-0 mr-4">
+                    <div class="symbol-label" style="background-image: url('media/stock-600x400/img-17.jpg')"></div>
+                  </div>
+                </td>
+                <td class="pl-0">
+                  <a href="#" class="text-dark font-weight-bolder text-hover-primary mb-1 font-size-lg">Post Title</a>
+                  <span class="text-muted font-weight-bold d-block">post extract</span>
+                </td>
+                <td class="text-right pr-0">
+                  <span class="text-dark-75 font-weight-bolder d-block font-size-lg">Category</span>
+                </td>
+                <td class="text-right">
+                  <span class="text-muted font-weight-bold">tag, tag, tag</span>
+                </td>
+                <td class="text-right">
+                  <span class="label label-lg label-light-danger label-inline">status</span>
+                </td>
+                <td class="text-center">
+                  <span class="label label-lg label-light-primary label-inline font-weight-bolder font-size-lg">56</span>
+                </td>
+                <td class="pr-0 text-right">
+                  <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm">
+                    <span class="svg-icon svg-icon-md svg-icon-primary">
+                      <!--begin::Svg Icon | path:assets/media/svg/icons/General/Settings-1.svg-->
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <rect x="0" y="0" width="24" height="24" />
+                          <path d="M7,3 L17,3 C19.209139,3 21,4.790861 21,7 C21,9.209139 19.209139,11 17,11 L7,11 C4.790861,11 3,9.209139 3,7 C3,4.790861 4.790861,3 7,3 Z M7,9 C8.1045695,9 9,8.1045695 9,7 C9,5.8954305 8.1045695,5 7,5 C5.8954305,5 5,5.8954305 5,7 C5,8.1045695 5.8954305,9 7,9 Z" fill="#000000" />
+                          <path 
+                          d="M7,13 L17,13 C19.209139,13 21,14.790861 21,17 C21,19.209139 19.209139,21 17,21 L7,21 C4.790861,21 3,19.209139 3,17 C3,14.790861 4.790861,13 7,13 Z M17,19 C18.1045695,19 19,18.1045695 19,17 C19,15.8954305 18.1045695,15 17,15 C15.8954305,15 15,15.8954305 15,17 C15,18.1045695 15.8954305,19 17,19 Z" 
+                          fill="#000000" opacity="0.3" />
+                        </g>
+                      </svg>
+                      <!--end::Svg Icon-->
+                    </span>
+                  </a>
+                  <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm mx-3">
+                    <span class="svg-icon svg-icon-md svg-icon-primary">
+                      <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Write.svg-->
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <rect x="0" y="0" width="24" height="24" />
+                          <path 
+                          d="M12.2674799,18.2323597 L12.0084872,5.45852451 C12.0004303,5.06114792 12.1504154,4.6768183 12.4255037,4.38993949 L15.0030167,1.70195304 L17.5910752,4.40093695 
+                          C17.8599071,4.6812911 18.0095067,5.05499603 18.0083938,5.44341307 L17.9718262,18.2062508 C17.9694575,19.0329966 17.2985816,19.701953 16.4718324,19.701953 L13.7671717,19.701953 C12.9505952,19.701953 12.2840328,19.0487684 12.2674799,18.2323597 Z" 
+                          fill="#000000" fill-rule="nonzero" transform="translate(14.701953, 10.701953) rotate(-135.000000) translate(-14.701953, -10.701953)" />
+                          <path 
+                          d="M12.9,2 C13.4522847,2 13.9,2.44771525 13.9,3 C13.9,3.55228475 13.4522847,4 12.9,4 L6,4 C4.8954305,4 4,4.8954305 4,6 L4,18 C4,19.1045695 4.8954305,20 6,20 L18,20 C19.1045695,20 20,19.1045695 20,18 L20,13 C20,12.4477153 20.4477153,12 21,12 
+                          C21.5522847,12 22,12.4477153 22,13 L22,18 C22,20.209139 20.209139,22 18,22 L6,22 C3.790861,22 2,20.209139 2,18 L2,6 C2,3.790861 3.790861,2 6,2 L12.9,2 Z" 
+                          fill="#000000" fill-rule="nonzero" opacity="0.3" />
+                        </g>
+                      </svg>
+                      <!--end::Svg Icon-->
+                    </span>
+                  </a>
+                  <a href="#" class="btn btn-icon btn-light btn-hover-primary btn-sm">
+                    <span class="svg-icon svg-icon-md svg-icon-primary">
+                      <!--begin::Svg Icon | path:assets/media/svg/icons/General/Trash.svg-->
+                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <rect x="0" y="0" width="24" height="24" />
+                          <path d="M6,8 L6,20.5 C6,21.3284271 6.67157288,22 7.5,22 L16.5,22 C17.3284271,22 18,21.3284271 18,20.5 L18,8 L6,8 Z" fill="#000000" fill-rule="nonzero" />
+                          <path d="M14,4.5 L14,4 C14,3.44771525 13.5522847,3 13,3 L11,3 C10.4477153,3 10,3.44771525 10,4 L10,4.5 L5.5,4.5 C5.22385763,4.5 5,4.72385763 5,5 L5,5.5 C5,5.77614237 5.22385763,6 5.5,6 L18.5,6 C18.7761424,6 19,5.77614237 19,5.5 L19,5 C19,4.72385763 18.7761424,4.5 18.5,4.5 L14,4.5 Z" fill="#000000" opacity="0.3" />
+                        </g>
+                      </svg>
+                      <!--end::Svg Icon-->
+                    </span>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TagData",
+  data () {
+    return {
+      //isTagSelected: false,
+    }
+  },
+  methods: {
+    
+  }
+}
+</script>
